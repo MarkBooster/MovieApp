1f43df063a93bfaa3731fbe3b7c60bbbb2b056b1
Mark van Winden <MarkvanWinden@mbp-van-mark.wireless.hhs.nl>
2016-07-12 12:38:07 +0200

    Created the maind and detail VC.

---
+++ MovieApp/Cell.swift
@@ -0,0 +1,24 @@
+//
+//  Cell.swift
+//  MovieApp
+//
+//  Created by Mark van Winden on 12-07-16.
+//  Copyright © 2016 Mark van Winden. All rights reserved.
+//
+
+import UIKit
+
+class Cell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ MovieApp/MainVC.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Mark van Winden on 12-07-16.
+//  Copyright © 2016 Mark van Winden. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        movies.append("Test")
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        movies.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        <#code#>
+    }
+
+
+
+
+}
+
